8ef6f0b77e091a183087259659ced9a06132160e
HyunWoong <68489467+hwinkr@users.noreply.github.com>
2022-09-10 08:30:53 +0900

    Delete toyProject directory

--- toyProject/Youtube/main.js
+++
@@ -1,6 +0,0 @@
- //html에서 특정한 무언가를 가져옴.
-const btnTouch = document.querySelector('.info .action .touch');
-const btnTouch1 = document.querySelector('.info .action .touch1')
-btnTouch.addEventListener('click',()=>{
-    btnTouch.classList.toggle('clicked');
- });
